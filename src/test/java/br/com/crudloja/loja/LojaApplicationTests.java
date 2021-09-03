7e6dea299f5aa262ce99992a6a1e3207a74cdbb2
Luiz Eduardo <programadorlesd@gmail.com>
2021-09-03 20:45:26 -0300

    Inicio

---
+++ src/test/java/br/com/crudloja/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crudloja.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
